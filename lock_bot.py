9a34367c1b4f4856e9bf195b763bf92e65e7fea8
Mandalore The Legendary <swmaster917@gmail.com>
2021-08-10 22:55:40 -0500

    bot is able to tag people now.

--- lock_bot.py
+++ lock_bot.py
@@ -13,7 +13,9 @@ async def on_message(message):
 
     if message.content.startswith('ch.in'):
         login_user = message.author
-        await message.channel.send(login_user + ' has logged in.')
+        login_user_mention = message.author.mention
+        await message.channel.send(str(login_user) + ' has logged in.')
+        await message.channel.send(login_user_mention + ': test to see if this creates a mention.')
 
 #Need to create a .env file and environment variable for token for security
 client.run('ODcxOTE3OTg4NjM3NDYyNTQ4.YQiS5w.dRJ54g6dzmQY-dJcGuus984c2A0')
